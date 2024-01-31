b1fcb9a989de88baa2c5b12d33308c15712289b1
Sergei Garcia <sgarcia.dev@gmail.com>
2024-01-30 19:15:10 -0600

    feat: add tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(44, 75, 255)',
+        'primary-faded': 'rgb(107, 129, 255)',
+        secondary: 'rgb(233, 114, 132)',
+      }
+    },
+  },
+  plugins: [],
+}
